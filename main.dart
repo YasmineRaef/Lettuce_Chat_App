ee6ed02a79b0cc3be80f78c3c901d968513db0c8
YasmineRaef <yasmineraef76@gmail.com>
2024-07-06 14:46:46 +0300

    adding missing retrieve

--- main.dart
+++ main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:lib_getx_template/app/app.dart';
+import 'package:lib_getx_template/data/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
+  retrieveData();
   runApp(const App());
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 }
